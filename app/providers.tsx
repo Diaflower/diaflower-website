d368653160ddb96a5921e4a9a861b82c586134c2
rami <rami@leadvy.com>
2024-08-02 01:59:38 +0400

    added Zustand and React Query

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,23 @@
 'use client';
 
 import {NextIntlClientProvider} from 'next-intl';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
 
 export function Providers({children, locale, messages}: {
   children: React.ReactNode;
   locale: string;
   messages: any;
 }) {
+
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
+
+    <QueryClientProvider client={queryClient}>
     <NextIntlClientProvider timeZone='Asia/Dubai' locale={locale} messages={messages}>
       {children}
     </NextIntlClientProvider>
+    </QueryClientProvider>
   );
 }
