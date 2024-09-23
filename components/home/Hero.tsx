99a7d7d15bdc02a885409aaf501c52b43a5d5b72
raminour <raminoreldaim@gmail.com>
2024-09-23 20:38:58 +0400

    edot video

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -9,14 +9,14 @@ export default function Hero() {
 
   return (
     <section className="w-full xl:container mx-auto relative mb-10">
-      <div className="relative w-full h-[200px] md:h-[400px] lg:h-[650px] overflow-hidden">
+      <div className="relative w-full h-[200px] md:h-[400px] lg:h-[550px] overflow-hidden">
         <video
           ref={videoRef}
           autoPlay
           loop
           muted
           playsInline
-          className="absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2"
+          className="absolute top-1/2 left-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2"
         >
           <source src="/newvid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
