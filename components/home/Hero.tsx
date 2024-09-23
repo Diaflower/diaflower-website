fc67852a337e6db2436258f0b1ca002cad30bc10
raminour <raminoreldaim@gmail.com>
2024-09-23 19:39:59 +0400

    video

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,21 +1,27 @@
-import Image from 'next/image'
-import HeroImage from '../../public/Banner-main-min.jpg'
+'use client'
+
+import { useRef, useEffect } from 'react'
 
 export default function Hero() {
-    return (
-        <section className="w-full xl:container mx-auto relative mb-10">
-            <div className="relative w-full h-[200px] md:h-[400px] lg:h-[500px]">
-                <Image
-                priority
-                src={HeroImage}
-                alt= 'test'
-                quality={85}
-                fill
-                sizes="(max-width: 768px) 100vw, 100vw"
-                className="object-cover"
-                />
-            </div>
-        </section>
-      
-    )
-  }
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+
+
+  return (
+    <section className="w-full xl:container mx-auto relative mb-10">
+      <div className="relative w-full h-[200px] md:h-[400px] lg:h-[650px] overflow-hidden">
+        <video
+          ref={videoRef}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-1/2 left-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2"
+        >
+          <source src="/vid1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </section>
+  )
+}
