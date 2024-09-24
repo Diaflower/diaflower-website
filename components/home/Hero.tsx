780096a2550a0aad1c83efb96f7c1514084b96ea
raminour <raminoreldaim@gmail.com>
2024-09-24 20:19:22 +0400

    fix

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
           ref={videoRef}
           autoPlay
           loop
-      
+          muted
           playsInline
           className="absolute top-1/2 left-1/2 min-w-full min-h-full object-cover -translate-x-1/2 -translate-y-1/2"
         >
