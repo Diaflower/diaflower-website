7ddcc601292ab835613ac079f0b4dd26552cab33
raminour <raminoreldaim@gmail.com>
2024-10-03 17:58:18 +0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const clerkPubkey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
@@ -8,6 +9,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
+      <GoogleAnalytics gaId="G-22RDMDJBN1" />
         <ClerkProvider publishableKey={clerkPubkey}>
           {children}
         </ClerkProvider>
